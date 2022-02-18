20d58c6b2ed20d795eb08960689933e988e5bde8
robyzzz <robyawyx@gmail.com>
2022-02-18 04:23:22 +0000

    project fix

---
+++ csl/pages/profile/_steamid.vue
@@ -0,0 +1,26 @@
+<template></template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    steamid: "", // steamid64
+  }),
+
+  async mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+      setTimeout(() => this.$nuxt.$loading.finish(), 3000);
+    });
+
+    if (this.$route.params.steamid == undefined) {
+      // TODO: if user is not logged in, we can not redirect him to his page, so we must avoid that
+      this.steamid = "1" /*state / local storage*/;
+    } else {
+      this.steamid = this.$route.params.steamid;
+    } 
+  },
+};
+</script>
+
+<style>
+</style>
