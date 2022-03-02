fa56aa2e11394162e2b4e1a02a86d330476ec960
robyzzz <robyawyx@gmail.com>
2022-03-02 18:57:04 +0000

    room websocket fixing

--- pages/room/[id].vue
+++ pages/room/[id].vue
@@ -1,22 +1,22 @@
 <template>
-  <h1>room {{ roomID }} </h1>
-  <div>
-			<table></table>
-			<fieldset>
-				<legend>Enter your message..</legend>
-				<div>
-					<input type="text" placeholder="Your message..." style="height:60px" v-model="chatMessage" />
-          <button v-on:click="onSendMsg">Send message</button>
-          <button v-on:click="onLeaveRoom">Leave room</button>
-				</div>
-			</fieldset>
-      <h2> chat: </h2>
-      <ul>
-        <li :key="m" v-for="m in getChatMessages">
-          {{m}}
-        </li>
-      </ul>
-	</div>
+  <div v-if="roomID != 0">
+    <h1>room {{ roomID }}</h1>
+    <table></table>
+    <fieldset>
+      <legend>Enter your message..</legend>
+      <div>
+        <input type="text" placeholder="Your message..." style="height: 60px" v-model="chatMessage" />
+        <button v-on:click="onSendMsg">Send message</button>
+        <button v-on:click="onLeaveRoom">Leave room</button>
+      </div>
+    </fieldset>
+    <h2>chat:</h2>
+    <ul>
+      <li :key="m" v-for="m in chat">
+        {{ m }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,73 +25,81 @@ export default defineNuxtComponent({
     return {
       roomID: 0 as number,
       ws: null as WebSocket, // room websocket
-      chatMessage: "" as string,
-      chat: [""] as string[], // TODO: as ChatMessage maybe with objects (sender, message, etc..) instead of strings.
+      chatMessage: '' as string,
+      chat: [] as string[], // TODO: as ChatMessage struct maybe with objects (sender, message, etc..) instead of hardcoded strings.
       steamid: (Math.floor(Math.random() * 10000) + 7000).toString(), // TODO: local storage steamid ofc
     }
   },
-  computed: {
-    getChatMessages: function (): string[] {
-      return this.chat; //FIXME: chat doesnt update?
-    }
-  },
   methods: {
-    onSendMsg: function () {
-      this.ws.send(JSON.stringify({
-        action: 'room-msg',
-        message: this.chatMessage,
-        sender: this.steamid,
-        //created: 'today', //TODO: today date? doesnt rly matter tho
-      }))
+    onSendMsg: function (): void {
+      this.ws.send(
+        JSON.stringify({
+          action: 'room-msg',
+          message: this.chatMessage,
+          sender: this.steamid,
+        })
+      )
     },
-    onReceiveMsg: function (msg) {
+    onReceiveMsg: function (msg): void {
+      var msg = JSON.parse(msg)
       console.log(msg)
-      // handle message action... if room-msg ... else if ... 
+
+      switch (msg.action) {
+        case "room-msg": {
+          this.chat.push(`${msg.sender} : ${msg.message}`)
+          break
+        }
+        case "join-room": {
+          this.chat.push(`${msg.sender} has entered the room.`)
+          break
+        }
+        case "left-room": {
+          this.chat.push(`${msg.sender} has left the room.`)
+          break
+        }
+      }
     },
-    onLeaveRoom: function () {
-      this.$router.push("/rooms");
+    onLeaveRoom: function (): void {
+      this.ws.close(1000, 'Client has left the room')
+      this.$router.push('/rooms')
+    },
+    //* Socket stuff
+    onSocketConnect: function (): void {
+    },
+    onSocketClose: function (): void {
+    },
+    onSocketMessage: function (evt): void {
+      this.onReceiveMsg(evt.data)
+    },
+    onSocketError: function (evt): void {
+      this.$router.push('/404') // TODO: 404 page showing the error ig
     },
   },
   mounted() {
-    const route = useRoute();
+    const route = useRoute()
 
-    const id = Number.parseInt(route.params.id as string, 10);
-    if (id == undefined || isNaN(id) /*|| room id does not exist */) {
-      this.$router.push("/404");
-      console.log('nan')
+    const id = Number.parseInt(route.params.id as string, 10)
+    if (id == undefined || isNaN(id) /*|| TODO: room id does not exist */) {
+      return this.$router.push('/404')
     }
 
-    this.roomID = id;
+    this.roomID = id
+
+    // connect to room websocket
     this.ws = new WebSocket(`ws://localhost:8081/api/room/${this.roomID}?steamid=${this.steamid}`)
-
+    const _m = this // refers vue methods
     this.ws.onopen = function () {
-      alert('Connected...')
+      _m.onSocketConnect()
     }
-
-    this.ws.onmessage = function (evt) {
-      //this.onReceiveMsg(evt.data) //FIXME: this.onReceiveMsg is not a function ?
-      // this code should be on onReceiveMsg:
-      var msg = JSON.parse(evt.data);
-      console.log(msg)
-
-      if (msg.action == "room-msg") {
-        //this.chat.push(`${msg.sender}: ${msg.message}`) //FIXME: Cannot read properties of undefined (reading 'push') ?
-        // This is how I fixed it but doesnt look like the right solution:
-        let arr = this.chat || []
-        arr.push(msg.message)
-        this.chat = arr
-      }
-    }
-
     this.ws.onclose = function () {
-      alert('Connection is closed...')
+      _m.onSocketClose()
     }
-
-    this.ws.onerror = function (err) {
-      this.$router.push("/404"); // TODO: 404 page
-      console.dir(err)
+    this.ws.onmessage = function (evt) {
+      _m.onSocketMessage(evt)
     }
-
+    this.ws.onerror = function (evt) {
+      _m.onSocketError(evt)
+    }
   },
 })
 </script>
